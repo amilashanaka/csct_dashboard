3f566bcb87826de60268fccf648a829602bf4fd4
amilashanaka <dsa.amilashanaka@gmail.com>
2022-12-26 12:42:43 +0000

    add charts

--- dash_board.php
+++ dash_board.php
@@ -12,19 +12,31 @@
         <div class="container-fluid">
             <?php include_once './info_box.php';?>
 
-          <div class="row">
+            <div class="row">
 
-            <div class="col-md-12">
+                <div class="col-md-12">
 
-            <div style="width:full; height:300px;" id="chart1"></div>
+                    <div style="width:full; height:400px;" id="chart1"></div>
+
+                </div>
+
+                <div class="col-4 col-md-4">
+
+                    <div style="width:full; height:400px;" id="chart2"></div>
+                </div>
+
+                <div class="col-8 col-md-8">
+
+                    <div style="width:full; height:400px;" id="chart3"></div>
+                </div>
 
             </div>
 
-          </div>
-          
 
-           
+
             <script src="dist/js/chart1.js" type="text/javascript"> </script>
+            <script src="dist/js/chart2.js" type="text/javascript"> </script>
+            <script src="dist/js/chart3.js" type="text/javascript"> </script>
 
 
 
---
+++ dist/js/chart2.js
@@ -0,0 +1,59 @@
+var dom = document.getElementById('chart2');
+            var myChart = echarts.init(dom, null, {
+                renderer: 'canvas',
+                useDirtyRect: false
+            });
+            var app = {};
+
+            var option2;
+
+            option2 = {
+                tooltip: {
+                  trigger: 'item'
+                },
+                legend: {
+                  top: '5%',
+                  left: 'center'
+                },
+                series: [
+                  {
+                    name: 'Access From',
+                    type: 'pie',
+                    radius: ['40%', '70%'],
+                    avoidLabelOverlap: false,
+                    itemStyle: {
+                      borderRadius: 10,
+                      borderColor: '#fff',
+                      borderWidth: 2
+                    },
+                    label: {
+                      show: false,
+                      position: 'center'
+                    },
+                    emphasis: {
+                      label: {
+                        show: true,
+                        fontSize: 40,
+                        fontWeight: 'bold'
+                      }
+                    },
+                    labelLine: {
+                      show: false
+                    },
+                    data: [
+                      { value: 1048, name: 'Search Engine' },
+                      { value: 735, name: 'Direct' },
+                      { value: 580, name: 'Email' },
+                      { value: 484, name: 'Union Ads' },
+                      { value: 300, name: 'Video Ads' }
+                    ]
+                  }
+                ]
+              };
+
+
+            if (option2 && typeof option2 === 'object') {
+                myChart.setOption(option2);
+            }
+
+            window.addEventListener('resize', myChart.resize);
